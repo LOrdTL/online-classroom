9d3b72b7c0529f377995f54d72c5f21527180e51
LOrdTL <124559861+LOrdTL@users.noreply.github.com>
2025-05-19 19:48:53 +0700

    Add files via upload

---
+++ project-frontend/src/api/socketConfig.js
@@ -0,0 +1,13 @@
+import { io } from "socket.io-client";
+
+// lay token tu local storage
+const token = localStorage.getItem("token");
+
+const socket = io("http://localhost:3000", {
+  transports: ["websocket"],
+  query: {
+    token: token,
+  },
+});
+// Xuất instance socket để sử dụng ở các file khác
+export default socket;
